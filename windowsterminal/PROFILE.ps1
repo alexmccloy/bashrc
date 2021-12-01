6fbcb343a9dfc375de7e474811623d087acbef3e
Alex McCloy <alex.mccloy@transmax.com.au>
2021-12-02 08:57:57 +1000

    Added windows terminal profiles

---
+++ windowsterminal/PROFILE.ps1
@@ -0,0 +1,3 @@
+Import-Module posh-git
+# oh-my-posh --init --shell pwsh --config C:/Users/amccloy/AppData/Local/Programs/oh-my-posh/themes/agnoster.omp.json | Invoke-Expression
+oh-my-posh --init --shell pwsh --config C:/Users/amccloy/AppData/Local/Programs/oh-my-posh/themes/alex.omp.json | Invoke-Expression
